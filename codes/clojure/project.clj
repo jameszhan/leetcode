991479bbd5a5e163bc91a77a606885267903e16a
James Zhan <zhiqiangzhan@gmail.com>
2021-11-07 03:58:30 +0800

    upgrade the version

--- codes/clojure/project.clj
+++ codes/clojure/project.clj
@@ -1,10 +1,10 @@
 (defproject leetcode "0.1.0-SNAPSHOT"
   :description "LeetCode Online Judge"
   :url "https://github.com/jameszhan/leetcode"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.3"]]
   :main ^:skip-aot leetcode.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
-
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
